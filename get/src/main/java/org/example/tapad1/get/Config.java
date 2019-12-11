8eb4099f52a4f3b4bfd7f2c5bac56abd3dee65d5
zhenik <nikita.zhevnitskiy@gmail.com>
2019-12-11 22:16:30 +0100

    add config, ent, docker-compose

---
+++ get/src/main/java/org/example/tapad1/get/Config.java
@@ -0,0 +1,29 @@
+package org.example.tapad1.get;
+
+import com.typesafe.config.ConfigFactory;
+import java.util.UUID;
+
+public class Config {
+  public final String name;
+  public final String kafkaBootstrapServers;
+  public final String topic;
+  public final String host;
+  public final int port;
+
+  private Config(String kafkaBootstrapServers, String topic, String host, int port) {
+    this.name = UUID.randomUUID().toString();
+    this.kafkaBootstrapServers = kafkaBootstrapServers;
+    this.topic = topic;
+    this.host = host;
+    this.port = port;
+  }
+
+  public static Config loadConfiguration() {
+    final com.typesafe.config.Config loadedConf = ConfigFactory.load().getConfig("app");
+    final String kafkaBootstrapServers = loadedConf.getString("kafka-bootstrap-servers");
+    final String topic = loadedConf.getString("topic");
+    final String host = loadedConf.getString("host");
+    final int port = loadedConf.getInt("port");
+    return new Config(kafkaBootstrapServers, topic, host, port);
+  }
+}
